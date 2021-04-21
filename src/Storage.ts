b7c026d422b4567794f523168de0a2e56ff3339b
Angelo Breuer <angelobr@outlook.de>
2021-04-21 22:54:09 +0200

    Improve sorting

--- src/Storage.ts
+++ src/Storage.ts
@@ -1,6 +1,35 @@
-import { Citation } from "./types/Citation"
+import { Citation, Person } from "./types/Citation"
 
 let citationsCache: Citation[] | undefined
+
+function comparePersons(a: Person, b: Person, reverse: boolean) {
+
+    let result = (reverse ? a.lastName : a.firstName)
+        .localeCompare((reverse ? b.lastName : b.firstName))
+
+    if (result !== 0) {
+        return result
+    }
+
+    result = (reverse ? a.firstName : a.lastName)
+        .localeCompare((reverse ? b.firstName : b.lastName))
+
+    return result
+}
+
+function comparer(a: Citation, b: Citation): number {
+
+    // compare authors
+    for (let index = 0; index < a.contributors.length, index < b.contributors.length; index++) {
+        let result
+        if ((result = comparePersons(a.contributors[index], b.contributors[index], index === 0)) !== 0) {
+            return result
+        }
+    }
+
+    // compare title
+    return a.title.localeCompare(b.title)
+}
 
 export function getCitations(): Citation[] {
     if (citationsCache) {
@@ -9,7 +38,7 @@ export function getCitations(): Citation[] {
 
     const data = localStorage.getItem('data')
     citationsCache = ((data ? JSON.parse(data) : [])) || []
-    citationsCache!.sort((a, b) => a.title.localeCompare(b.title))
+    citationsCache!.sort(comparer)
     return citationsCache!
 }
 
